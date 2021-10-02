5afe8ec6b8dc530005edfa23f1e9b6d5fcf31b6e
genesissiana <perezgenesisa@gmail.com>
2021-10-01 18:21:40 -0700

    refractor code

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,22 +1,21 @@
 import React, { createContext, useContext } from "react";
-import { useProductReducer } from './reducers';
+ import { useProductReducer } from './reducers';
 
-// instantiate the global state object
-const StoreContext = createContext();
-const { Provider } = StoreContext;
+ const StoreContext = createContext();
+ const { Provider } = StoreContext;
 
-const StoreProvider = ({ value = [], ...props }) => {
-    const [state, dispatch] = useProductReducer({
-        products: [],
-        categories: [],
-        currentCategory: '',
-    });
-    console.log(state);
-    return <Provider value={[state, dispatch]} {...props} />;
-};
+ const StoreProvider = ({ value = [], ...props }) => {
+     const [state, dispatch] = useProductReducer({
+         products: [],
+         categories: [],
+         currentCategory: '',
+     });
+     console.log(state);
+     return <Provider value={[state, dispatch]} {...props} />;
+ };
 
-const useStoreContext = () => {
-    return useContext(StoreContext);
-};
+ const useStoreContext = () => {
+     return useContext(StoreContext);
+ };
 
-export { StoreProvider, useStoreContext };
+ export { StoreProvider, useStoreContext };
