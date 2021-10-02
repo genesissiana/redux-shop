858990332298671f4e09f1739cbf81ef341a76b7
genesissiana <perezgenesisa@gmail.com>
2021-10-01 19:24:12 -0700

    reinstall dependencies

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,21 +1,26 @@
+
+   
 import React, { createContext, useContext } from "react";
- import { useProductReducer } from './reducers';
+import { useProductReducer } from './reducers';
 
- const StoreContext = createContext();
- const { Provider } = StoreContext;
+// instantiate the global state object
+const StoreContext = createContext();
+const { Provider } = StoreContext;
 
- const StoreProvider = ({ value = [], ...props }) => {
-     const [state, dispatch] = useProductReducer({
-         products: [],
-         categories: [],
-         currentCategory: '',
+const StoreProvider = ({ value = [], ...props }) => {
+   const [state, dispatch] = useProductReducer({
+       products: [],
+       cart: [],
+       cartOpen: false,
+       categories: [],
+       currentCategory: ''
      });
-     console.log(state);
-     return <Provider value={[state, dispatch]} {...props} />;
- };
+    console.log(state);
+    return <Provider value={[state, dispatch]} {...props} />;
+};
 
- const useStoreContext = () => {
-     return useContext(StoreContext);
- };
+const useStoreContext = () => {
+    return useContext(StoreContext);
+};
 
- export { StoreProvider, useStoreContext };
+export { StoreProvider, useStoreContext };
